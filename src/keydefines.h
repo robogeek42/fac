b56e53411a9cf797deace5945f6eaa93eee4c0bb
Assif <assif42@gmail.com>
2024-03-23 10:04:19 +0000

    add keydefines.h

---
+++ src/keydefines.h
@@ -0,0 +1,137 @@
+#ifndef _KEYDEFINES_H
+#define _KEYDEFINES_H
+
+#define KEY_space 0x01
+
+#define KEY_0 0x02
+#define KEY_1 0x03
+#define KEY_2 0x04
+#define KEY_3 0x05
+#define KEY_4 0x06
+#define KEY_5 0x07
+#define KEY_6 0x08
+#define KEY_7 0x09
+#define KEY_8 0x0A
+#define KEY_9 0x0B
+
+#define KEY_a 0x16
+#define KEY_b 0x17
+#define KEY_c 0x18
+#define KEY_d 0x19
+#define KEY_e 0x1A
+#define KEY_f 0x1B
+#define KEY_g 0x1C
+#define KEY_h 0x1D
+#define KEY_i 0x1E
+#define KEY_j 0x1F
+#define KEY_k 0x20
+#define KEY_l 0x21
+#define KEY_m 0x22
+#define KEY_n 0x23
+#define KEY_o 0x24
+#define KEY_p 0x25
+#define KEY_q 0x26
+#define KEY_r 0x27
+#define KEY_s 0x28
+#define KEY_t 0x29
+#define KEY_u 0x2A
+#define KEY_v 0x2B
+#define KEY_w 0x2C
+#define KEY_x 0x2D
+#define KEY_y 0x2E
+#define KEY_z 0x2F
+
+#define KEY_A 0x30
+#define KEY_B 0x31
+#define KEY_C 0x32
+#define KEY_D 0x33
+#define KEY_E 0x34
+#define KEY_F 0x35
+#define KEY_G 0x36
+#define KEY_H 0x37
+#define KEY_I 0x38
+#define KEY_J 0x39
+#define KEY_K 0x3A
+#define KEY_L 0x3B
+#define KEY_M 0x3C
+#define KEY_N 0x3D
+#define KEY_O 0x3E
+#define KEY_P 0x3F
+#define KEY_Q 0x40
+#define KEY_R 0x41
+#define KEY_S 0x42
+#define KEY_T 0x43
+#define KEY_U 0x44
+#define KEY_V 0x45
+#define KEY_W 0x46
+#define KEY_X 0x47
+#define KEY_Y 0x48
+#define KEY_Z 0x49
+#define KEY_backtick 0x4A
+
+#define KEY_minus 0x4F
+#define KEY_underscore 0x70
+
+#define KEY_equals 0x4E
+#define KEY_plus 0x51
+
+#define KEY_lcurlybrace 0x68
+#define KEY_lsquarebrace 0x6A
+#define KEY_rcurlybrace 0x69
+#define KEY_rsquarebrace 0x6B
+
+#define KEY_semicolon 0x5C
+#define KEY_colon 0x5A
+
+#define KEY_tick 0x6C
+#define KEY_at 0x60
+
+#define KEY_hash 0x7F
+#define KEY_tilda 0x73
+
+#define KEY_forwardslash 0x57
+#define KEY_questionmark 0x67
+
+#define KEY_backslash 0x55
+#define KEY_pipe 0x5E
+
+#define KEY_comma 0x5B
+#define KEY_dot 0x59
+#define KEY_lt 0x6E
+#define KEY_gt 0x6F
+
+#define KEY_shift 0x76
+#define KEY_alt 0x77
+#define KEY_altgraphic 0x79
+#define KEY_lcontrol 0x79
+#define KEY_rcontrol 0x7A
+
+#define KEY_insert 0x80
+#define KEY_delete 0x82
+#define KEY_backspace 0x84
+#define KEY_home 0x85
+#define KEY_end 0x87
+#define KEY_tab 0x8F
+#define KEY_enter 0x8F
+#define KEY_pageup 0x92
+#define KEY_pagedown 0x94
+
+#define KEY_LEFT 0x9A
+#define KEY_RIGHT 0x9C
+#define KEY_UP 0x96
+#define KEY_DOWN 0x98
+
+#define KEY_f1 0x9F
+#define KEY_f2 0xA0
+#define KEY_f3 0xA1
+#define KEY_f4 0xA2
+#define KEY_f5 0xA3
+#define KEY_f6 0xA4
+#define KEY_f7 0xA5
+#define KEY_f8 0xA6
+#define KEY_f9 0xA7
+#define KEY_f10 0xA8
+#define KEY_f11 0xA9
+#define KEY_f12 0xAA
+
+#endif
