b756dc00b05e9bf038a827acb2d43d7a4c0a2ea2
assif <assif42@gmail.com>
2024-03-27 08:51:14 +0000

    stab at furnace general struct

---
+++ src/machine.h
@@ -0,0 +1,30 @@
+/*
+  vim:ts=4
+  vim:sw=4
+*/
+#ifndef _MACHINE_H
+#define _MACHINE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "images.h"
+
+#define MACHINE_FURNACE 0
+
+typedef struct {
+	uint8_t machine;
+	uint8_t in_items[5];
+	uint8_t out_items[5];
+	int time;
+} Furnace;
+
+Furnace furnace[] = {
+	{ MACHINE_FURNACE, 
+		{ IT_STONE, IT_IRON_ORE, IT_COPPER_ORE, -1, -1 },
+   		{ IT_STONE_BRICK, IT_IRON_PLATE, IT_COPPER_PLATE, -1, -1 },
+		4
+	},
+};
+#endif
